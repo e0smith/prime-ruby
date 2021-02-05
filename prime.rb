1ebff07bdf68c062034d46b2710771064e288581
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-02-05 18:28:06 -0500

    commit tree d9a2daef7c8054152b0f8c68e5f50925286cb581

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require "pry"
+def prime?(num)
+    return false if num <= 1
+    prime = true
+    (2...(num)).to_a.each do |n| 
+        if num % n == 0 
+            prime = false
+        end
+    end
+    prime
+end
+    #(2..(num)).to_a {|n| num % n == 0}
+    #binding.pry
+    #false
